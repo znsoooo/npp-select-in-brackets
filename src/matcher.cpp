6fc0d3c0d6f3d5337a8456f9c4cf4ba2194a3404
Shixian Li <lsx7@sina.com>
2025-05-02 16:54:04 +0800

    Style: Move text parameters to global variables

--- src/matcher.cpp
+++ src/matcher.cpp
@@ -15,13 +15,17 @@
 
 typedef std::vector<std::vector<int>> Vec2d;
 
+static const TCHAR text_brackets[] = _TEXT("()[]{}<>''\"\"``（）［］｛｝《》「」『』【】〖〗‘’“”");
+static const TCHAR text_inline[] = _TEXT("'\"<");
+static const TCHAR text_eol[] = _TEXT("\r\n");
+
 
 static int FindMatch(const TCHAR* str, int pos, const int final_pos, const int add_ch, const int sub_ch, const bool in_line)
 {
     const int add = pos <= final_pos ? 1 : -1;
     for (int counter = 1; pos != final_pos; pos += add) {
         const int ch = str[pos];
-        if (in_line && _tcschr(_TEXT("\r\n"), ch)) {
+        if (in_line && _tcschr(text_eol, ch)) {
             return -1;
         }
         if (ch == sub_ch) {
@@ -41,15 +45,14 @@ static auto FindMatchingBracket(const TCHAR* str, const int length, const int se
     // find the matching brackets closest to the selected area.
     // if no found, select the whole text.
 
-    const TCHAR chars[] = _TEXT("()[]{}<>''\"\"``（）［］｛｝《》「」『』【】〖〗‘’“”");
-    const int types = _tcsclen(chars) / 2;
+    const int count = _tcsclen(text_brackets) / 2;
 
     bool found = false;
     int start = 0, end = length, distance = (length + 1) * 2;
-    for (int i = 0; i < types; i++) {
-        const int open_ch = chars[i * 2];
-        const int close_ch = chars[i * 2 + 1];
-        const bool in_line = _tcschr(_TEXT("'\"<"), open_ch);
+    for (int i = 0; i < count; i++) {
+        const int open_ch = text_brackets[i * 2];
+        const int close_ch = text_brackets[i * 2 + 1];
+        const bool in_line = _tcschr(text_inline, open_ch);
         int tmp_start = FindMatch(str, sel_start - 1, -1, close_ch, open_ch, in_line) + 1;
         int tmp_end = FindMatch(str, sel_end, length, open_ch, close_ch, in_line);
         int tmp_distance = min((sel_start - tmp_start) * 2, (tmp_end - sel_end) * 2 + 1);  // left char is more closer
