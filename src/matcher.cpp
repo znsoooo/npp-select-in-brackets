2003183c7c0ac8a86c61b23fa9150438b2b362dc
Shixian Li <lsx7@sina.com>
2025-05-04 14:22:17 +0800

    Refactor: Find matching brackets in literal order

--- src/matcher.cpp
+++ src/matcher.cpp
@@ -20,51 +20,65 @@ static const TCHAR text_inline[] = _TEXT("'\"<");
 static const TCHAR text_eol[] = _TEXT("\r\n");
 
 
-static int FindMatch(const TCHAR* str, int pos, const int final_pos, const int add_ch, const int sub_ch, const bool in_line)
+static bool CountChar(const TCHAR ch, const TCHAR add_ch, const TCHAR sub_ch, int& counter)
 {
-    const int add = pos <= final_pos ? 1 : -1;
-    for (int counter = 1; pos != final_pos; pos += add) {
-        const int ch = str[pos];
-        if (in_line && _tcschr(text_eol, ch)) {
-            return -1;
-        }
-        if (ch == sub_ch) {
-            counter--;
-        } else if (ch == add_ch) {
-            counter++;
-        }
-        if (counter == 0) {
-            return pos;
-        }
+    if (ch == sub_ch) {
+        counter--;
+    } else if (ch == add_ch) {
+        counter++;
     }
-    return -1;
+    if (counter == -1) {
+        return true;
+    }
+    return false;
 }
 
 static auto FindMatchingBracket(const TCHAR* str, const int length, const int sel_start, const int sel_end)
 {
     // find the matching brackets closest to the selected area.
     // if no found, select the whole text.
+    // left position is closer.
 
-    const int count = _tcsclen(text_brackets) / 2;
+    const int count = _tcsclen(text_brackets) & ~1;
 
-    bool found = false;
-    int start = 0, end = length, distance = (length + 1) * 2;
-    for (int i = 0; i < count; i++) {
-        const int open_ch = text_brackets[i * 2];
-        const int close_ch = text_brackets[i * 2 + 1];
-        const bool in_line = _tcschr(text_inline, open_ch);
-        int tmp_start = FindMatch(str, sel_start - 1, -1, close_ch, open_ch, in_line) + 1;
-        int tmp_end = FindMatch(str, sel_end, length, open_ch, close_ch, in_line);
-        int tmp_distance = min((sel_start - tmp_start) * 2, (tmp_end - sel_end) * 2 + 1);  // left char is more closer
-        if (tmp_start != 0 && tmp_end != -1 && tmp_distance < distance) {
-            found = true;
-            start = tmp_start;
-            end = tmp_end;
-            distance = tmp_distance;
+    int counter[count] = {0};
+
+    for (int idx_left = sel_start - 1, idx_right = sel_end; idx_left >= 0 || idx_right < length; idx_left--, idx_right++) {
+        if (idx_left >= 0) {
+            const TCHAR ch_left = str[idx_left];
+            for (int idx_br = 0; idx_br < count; idx_br += 2) {
+                const TCHAR open_ch = text_brackets[idx_br];
+                const TCHAR close_ch = text_brackets[idx_br + 1];
+                if (CountChar(ch_left, close_ch, open_ch, counter[idx_br])) {
+                    int counter2 = 0;
+                    for (int idx_right2 = sel_end; idx_right2 < length; idx_right2++) {
+                        const TCHAR ch_right2 = str[idx_right2];
+                        if (CountChar(ch_right2, open_ch, close_ch, counter2)) {
+                            return std::make_tuple(true, idx_left + 1, idx_right2);
+                        }
+                    }
+                }
+            }
+        }
+        if (idx_right < length) {
+            const TCHAR ch_right = str[idx_right];
+            for (int idx_br = 1; idx_br < count; idx_br += 2) {
+                const TCHAR open_ch = text_brackets[idx_br - 1];
+                const TCHAR close_ch = text_brackets[idx_br];
+                if (CountChar(ch_right, open_ch, close_ch, counter[idx_br])) {
+                    int counter2 = 0;
+                    for (int idx_left2 = sel_start - 1; idx_left2 >= 0; idx_left2--) {
+                        const TCHAR ch_left2 = str[idx_left2];
+                        if (CountChar(ch_left2, close_ch, open_ch, counter2)) {
+                            return std::make_tuple(true, idx_left2 + 1, idx_right);
+                        }
+                    }
+                }
+            }
         }
     }
 
-    return std::make_tuple(found, max(0, start), min(length, end));
+    return std::make_tuple(false, 0, length);
 }
 
 auto FindMatchingBrackets(const TCHAR* str, const int length, const Vec2d sels)
